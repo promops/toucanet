36b98f1ada01dd1e1fca829fa89077f2c0ee6659
kompotiq <flip.oleg@gmail.com>
2020-03-18 01:44:51 +0300

    Главная страница

---
+++ lib/app/pages/home_page/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>  with SingleTickerProviderStateMixin{
+  TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo,
+      bottomNavigationBar: TabBar(
+        controller: _tabController,
+        tabs: <Widget>[
+          Tab(
+            
+            icon: Icon(Icons.message),
+            text: 'Чаты',
+          ),
+           Tab(
+            icon: Icon(Icons.portrait),
+            text: 'Чаты',
+          )
+        ],
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [Container(), Container()])
+    );
+  }
+}
